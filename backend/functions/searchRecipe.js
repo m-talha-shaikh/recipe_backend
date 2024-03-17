4be8ce7dbd2ce2be22e8de3c15b994be4d45f8ba
Muhammad Talha Shaikh <m-talha-shaikh@protonmail.com>
2024-03-17 22:00:59 +0500

    wild card

--- backend/functions/searchRecipe.js
+++ backend/functions/searchRecipe.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "https://recipe-finder-black.vercel.app",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
       },
