92839bad62c307576a36dabc8d4a22c5498aad22
Muhammad Talha Shaikh <m-talha-shaikh@protonmail.com>
2024-03-18 02:49:30 +0500

    checking for star

--- backend/functions/searchRecipe.js
+++ backend/functions/searchRecipe.js
@@ -1,3 +1,47 @@
+// exports.handler = async (event, context) => {
+//   if (event.httpMethod === 'OPTIONS') {
+//     // Respond to preflight requests
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Headers": "Content-Type",
+//         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+//       },
+//       body: ''
+//     };
+//   }
+
+//   try {
+//     const { query } = JSON.parse(event.body);
+//     // Perform your recipe search logic here
+
+//     const recipes = [
+//       { title: "Recipe 1", image: "recipe1.jpg" },
+//       { title: "Recipe 2", image: "recipe2.jpg" },
+//       // Dummy recipe data, replace with actual logic
+//     ];
+
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Headers": "Content-Type",
+//         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+//       },
+//       body: JSON.stringify(recipes),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: "Internal Server Error" }),
+//     };
+//   }
+// };
+
+
+const axios = require('axios');
+
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'OPTIONS') {
     // Respond to preflight requests
@@ -14,13 +58,19 @@ exports.handler = async (event, context) => {
 
   try {
     const { query } = JSON.parse(event.body);
-    // Perform your recipe search logic here
 
-    const recipes = [
-      { title: "Recipe 1", image: "recipe1.jpg" },
-      { title: "Recipe 2", image: "recipe2.jpg" },
-      // Dummy recipe data, replace with actual logic
-    ];
+    // Make a request to the Spoonacular API
+    const apiKey = '408d436b50c74df3bf2c7f01db1f6cd6';
+    const baseUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+    const response = await axios.get(baseUrl, {
+      params: {
+        apiKey: apiKey,
+        query: query,
+      },
+    });
+
+    // Extract recipe data from the response
+    const recipes = response.data.results;
 
     return {
       statusCode: 200,
